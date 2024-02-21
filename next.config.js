b4c4bc59df9c35bebb49ed6de6b903c7572a0466
IreneAragon <irenearagongomez@gmail.com>
2024-02-21 12:58:03 +0100

    deploy on github pages

--- next.config.js
+++ next.config.js
@@ -1,4 +1,8 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {}
-
-module.exports = nextConfig
+const nextConfig = {
+    basePath: "/portfolio",
+    output: "export",
+    reactStrictMode: true,
+  };
+  
+  module.exports = nextConfig;
--- src/app/porfolio/[id]/page.jsx
+++
@@ -1,6 +0,0 @@
-export default function Project({params}) {
-    const {id} = params
-    return (
-      <h1>Esto es el proyecto con id: {id}</h1>
-    )
-}
